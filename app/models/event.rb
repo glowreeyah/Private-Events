5b34649bc3ba798be657d5044b5cd5b4474f7ef5
Glory David <glodave99@gmail.com>
2020-09-04 17:46:23 +0100

    Edit user's show page to display events to be attended by user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :date, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 300 }
 
-  belongs_to :creator, class_name: 'User'
+  belongs_to :creator,  foreign_key: :creator_id, class_name: 'User'
   has_many :invitations, foreign_key: :attended_event_id
-  has_many :attendees, through: :invitations
+  has_many :attendees, through: :invitations, source: :attendee
 
   scope :past, -> { where('date < ?', Date.today) }
   scope :future, -> { where('date >= ?', Date.today) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
   has_many :invitations, foreign_key: :attendee_id
-  has_many :attended_events, through: :invitations
+  has_many :attended_events, through: :invitations, source: :attended_event
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 11 }
 
   scope :past, -> { where('date < ?', Date.today) }
   scope :future, -> { where('date >= ?', Date.today) }
